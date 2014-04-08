73debad03271c1639283930d143bbf62cc3d0853
miketamburo <matsatam@gmail.com>
2014-04-08 13:45:43 -0500

    updated HomeController for register page

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -14,10 +14,47 @@ class HomeController extends BaseController {
 	|	Route::get('/', 'HomeController@showWelcome');
 	|
 	*/
-
-	public function showWelcome()
-	{
+	public function showWelcome(){
 		return View::make('hello');
+	}
+
+	public function showHome(){
+		return View::make('index');
+	}
+
+	public function showLogin() {
+		return View::make('login');
+	}
+
+	public function doLogin() {
+		// create the validator
+    	$validator = Validator::make(Input::all(), User::$signin_rules);
+
+    	// attempt validation
+    	if ($validator->fails()){
+        	// validation failed, redirect with validation errors
+        	$messages = $validator->messages();
+        	foreach ($messages->all() as $message){
+    			Session::flash('errorMessage', $message);	
+        	}
+    		return Redirect::back()->withInput()->withErrors($validator);
+    	} else {
+
+			// Perform Authentication
+			if (Auth::attempt(array('email' => Input::get('email'), 'password' => Input::get('password')))) {
+	    		return Redirect::intended('/posts');
+			} else {
+				Session::flash('errorMessage', 'User email or password not recognized.  Please try again.');
+				return Redirect::back()->withInput()->withErrors($validator);
+			}
+    		
+    	}
+
+	}
+
+	public function logout() {
+		Auth::logout();
+		return Redirect::action('PostsController@index');
 	}
 
 }
