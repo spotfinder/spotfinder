936348ac47a835c826a520ade864e513979c815d
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-04-09 09:21:21 -0500

    updating table fields

--- app/views/reservation.blade.php
+++ app/views/reservation.blade.php
@@ -25,19 +25,19 @@
 <div id = "formreserve">
 		<h2>Reserve Now</h2> 
 		{{ Form::open(array('action' => 'HomeController@showReservation', 'class' => 'form-signin', 'role'=>'form')) }}
-	    	<p>{{ Form::label('location', 'Location') }}
+	    	<p>{{ Form::label('area', 'Area') }}
 	    	   <select class="form-control">
-					<option>Downtown</option>
-					<option>Central Park</option>
+					<option value"1">Downtown</option>
+					<option value"2">Central Park</option>
 				</select> </p>
 	        <p>{{ Form::label('arrival_date', 'Arrival Date') }}
                <input type="date" class="form-control" id="arrival_date" placeholder=""></p>
-            <p>{{ Form::label('start_time', 'Start Time') }}
-               <input type="time" class="form-control" id="start_time" placeholder=""></p>
+            <p>{{ Form::label('arrival_time', 'Arrival Time') }}
+               <input type="time" class="form-control" id="arrival_time" placeholder=""></p>
             <p>{{ Form::label('departure_date', 'Departure Date') }}
                <input type="date" class="form-control" id="departure_date" placeholder=""></p>
-            <p>{{ Form::label('end_time', 'End Time') }}
-               <input type="time" class="form-control" id="end_time" placeholder=""></p>	
+            <p>{{ Form::label('departure_time', 'Departure Time') }}
+               <input type="time" class="form-control" id="departure_time" placeholder=""></p>	
 			<p>{{ Form::label('remember_me', 'Remember me!') }}
 			   {{ Form::checkbox('remember_me', 'value', true)}}<p>
 	        <p>{{ Form::submit('Submit', array('class' => 'btn btn-lg btn-primary btn-block')) }}</p>
