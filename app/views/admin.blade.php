998b0837b92b6b6ad6427deef094385173a6b3b6
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-04-13 15:51:49 -0500

    styling the admin view and added a master layout-admin

--- app/views/admin.blade.php
+++ app/views/admin.blade.php
@@ -1,80 +1,85 @@
-@extends('layouts.master-view')
-
+@extends('layouts.master-admin')
 @section('top-script')
-	<style type="text/css">
-		 h1 {
-			margin-top: 90px;
-			text-align: center;
+    <style>
+        #content{
 
-		}
-		th{
-			text-align: center;
-		}
-		body {
-			background-color: #4EA784;
-			background-image: url("/assets/images/patterns/pattern-1.png");
-		}	
-	</style>
+            margin-left: 50px;
+            
+        }
+        th{
+            text-align: center;
+        }
+        body {
+            background-color: #B30B30;
+            background-image: url("/assets/images/patterns/pattern-1.png");
+        }   
+    </style>
 @stop
-
 @section('content')
-	<div>
-		<h1>Admin Dashboard</h1>
-	</div>
 
-	<div class="container-fluid projects">
+<div class="container-fluid projects">
+        <div class="col-md-3"></div>
+        <div class="col-md-8">
+        <h1 class = "text-center">Users</h1>
 
-		<div class="col-md-2"></div>
+            <table class="table table-striped table-hover table-bordered ">
+                <tr>
+                    <th>
+                        First Name
+                    </th>
+                    <th>
+                        Last Name
+                    </th>
+                    <th>
+                        Email
+                    </th>
+                    <th>
+                        Created At
+                    </th>
+                    <th>
+                        Updated At
+                    </th>
+                    <th>
+                        <a><span class="glyphicon glyphicon-pencil"></span></a>
+                    </th>
+                    <th>
+                        <a><span class="glyphicon glyphicon-trash"></span></a>
+                    </th>
+                </tr>
+<!--                @Foreach ($users as $user)
+                <tr>
+                    <td>
+                        $user->first_name
+                    </td>
+                    <td>
+                        $user->last_name
+                    </td>
+                    <td>
+                        $user->email
+                    </td>
+                    <td>
+                        $user->created_at
+                    </td>
+                    <td>
+                        $user->updated_at
+                    </td>
+                </tr>
+                    End Foreach -->
+            </table>
 
-		<div class="col-md-8">
+        </div>
 
-			<table class="table table-striped table-hover table-bordered ">
-				<tr>
-					<th>
-						First Name
-					</th>
-					<th>
-						Last Name
-					</th>
-					<th>
-						Email
-					</th>
-					<th>
-						Created At
-					</th>
-					<th>
-						Updated At
-					</th>
-					<th>
-						<a><span class="glyphicon glyphicon-pencil"></span></a>
-					</th>
-					<th>
-						<a><span class="glyphicon glyphicon-trash"></span></a>
-					</th>
-				</tr>
-<!-- 				@Foreach ($users as $user)
-				<tr>
-					<td>
-						$user->first_name
-					</td>
-					<td>
-						$user->last_name
-					</td>
-					<td>
-						$user->email
-					</td>
-					<td>
-						$user->created_at
-					</td>
-					<td>
-						$user->updated_at
-					</td>
-				</tr>
-					End Foreach -->
-			</table>
-
-		</div>
-
-		<div class="col-md-2"></div>
-	</div>
+    <div class="col-md-2"></div>
+</div>
 @stop
+
+@section('bottom-script')
+    <!-- Custom JavaScript for the Menu Toggle -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("active");
+    });
+    </script>
+@stop
+
