bf81d7233794626082ea68904a02d9af27b65f81
miketamburo <matsatam@gmail.com>
2014-04-14 14:04:42 -0500

    created tables and synch'd reservation controller

--- app/database/migrations/2014_04_09_194949_create_users_table.php
+++ app/database/migrations/2014_04_09_194949_create_users_table.php
@@ -16,13 +16,20 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function($table)
 		{
 		    $table->increments('id')->unsigned();
+		    $table->string('customer_number', 25);
 		    $table->string('first_name', 50);
 		    $table->string('last_name', 50);
+		    $table->string('street_address', 100);
+		    $table->string('city', 100);
+		    $table->string('state', 100);
+		    $table->string('zip', 25);
+		    $table->string('phone_number', 35);
 		    $table->string('email', 200)->unique();
 		    $table->string('password', 100);
 		    $table->integer('role_id')->unsigned();
 		    $table->timestamps();
 		});
+
 	}
 
 	/**
