1371fa8daa70eb4b782c6fe9c7d7ed9a0e7296a4
miketamburo <matsatam@gmail.com>
2014-04-14 09:10:15 -0500

    version of reservation controller before adding a join to another database

--- app/controllers/ReservationController.php
+++ app/controllers/ReservationController.php
@@ -79,14 +79,16 @@ class ReservationController extends BaseController {
 		} else if (sizeof($resultsOfOpenSpaces) == 0) {
 			// query reservation table for parking spaces in the desired area where requested arrival time is after any already booked departure space times and where the desired departure time is before any already booked arrival times. 
 			$reservedSpaces = $this->searchReservations($requestedArea, $requestedArrivalDateTime, $requestedDepartureDateTime);
-
+var_dump($reservedSpaces);
+die;
     		// get results of reservations query and count them.  If query result is zero, send the user a message - sorry, there are no space that meet your criteria.  Please return to the reservation make to change your search criteria.
     		if (sizeof($reservedSpaces) == 0){
     			Session::flash('errorMessage', 'No available parking spaces matched your search criteria.  Please amend your search and try again.');
 				return Redirect::action('HomeController@showReservation')->withInput();
 			}
     	} else {
-
+var_dump($reservedSpaces);
+die;
     		// If reservation query is > zero, list results in a table for the user to review.  Pick only 5 and allow the user to pick only one choice to make a reservation.
 			Session::flash('successMessage', 'We found some options for you.');
 				// the results of the reservation search ($reservedSpaces) should be passed to the results (aka search) view
