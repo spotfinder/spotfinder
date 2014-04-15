54a6d76ff3ca8bf91458ddfa3fac2873774efc9f
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-04-15 09:55:53 -0500

    adding a datetimepicker: bower

--- app/views/layouts/master-admin.blade.php
+++ app/views/layouts/master-admin.blade.php
@@ -108,11 +108,11 @@
             <li class="dropdown user-dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> {{ $user->first_name }} <b class="caret"></b></a>
               <ul class="dropdown-menu">
-                <li><a href="#"><i class="fa fa-user"></i> Profile</a></li>
+                <li><a href="{{{ action('RegisterController@index') }}}"><i class="fa fa-user"></i> Profile</a></li>
                 <li><a href="#"><i class="fa fa-envelope"></i> Inbox <span class="badge">7</span></a></li>
                 <li><a href="#"><i class="fa fa-gear"></i> Settings</a></li>
                 <li class="divider"></li>
-                <li><a href="#"><i class="fa fa-power-off"></i> Log Out</a></li>
+                <li><a href="{{{ action('HomeController@logout') }}}"><i class="fa fa-power-off"></i> Log Out</a></li>
               </ul>
             </li>
           </ul>
--- app/views/reservation.blade.php
+++ app/views/reservation.blade.php
@@ -1,40 +1,41 @@
 @extends('layouts.master-view')
 
 @section('top-script')
-<style>
-	.form-styleReservation {
-  max-width: 330px;
-  padding: 15px;
-  margin: 0 auto;
-  margin-bottom: 10px;
-  font-weight: normal;
-  position: relative;
-  height: auto;
-  -webkit-box-sizing: border-box;
-     -moz-box-sizing: border-box;
-          box-sizing: border-box;
-  padding: 10px;
-  font-size: 16px;
-  z-index: 2;
-}
-
-	#formreserve {
-		margin-top: 100px;
-
-	
+	<link rel="stylesheet" href="/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css">
+	<style>
+		.form-styleReservation {
+	  max-width: 330px;
+	  padding: 15px;
+	  margin: 0 auto;
+	  margin-bottom: 10px;
+	  font-weight: normal;
+	  position: relative;
+	  height: auto;
+	  -webkit-box-sizing: border-box;
+	     -moz-box-sizing: border-box;
+	          box-sizing: border-box;
+	  padding: 10px;
+	  font-size: 16px;
+	  z-index: 2;
 	}
-		h2{
-			text-align: center;
+
+		#formreserve {
+			margin-top: 100px;
+
+		
 		}
-	body {
-		background-color: #4EA784;
-		background-image: url("/assets/images/patterns/pattern-1.png");
-	}
+			h2{
+				text-align: center;
+			}
+		body {
+			background-color: #4EA784;
+			background-image: url("/assets/images/patterns/pattern-1.png");
+		}
 
-	a{
-		color: #045BA5;
-	}
-</style>
+		a{
+			color: #045BA5;
+		}
+	</style>
 @stop
 
 @section('content')
@@ -48,15 +49,43 @@
 					<option value="3">Alamodome</option>
 					<option value="4">AT&amp;T Center</option>
 				</select> </p>
-	        <p>{{ Form::label('arrival_date', 'Arrival Date &amp; Time') }}
-               <input type="datetime-local" class="form-control" name='arrival_date_time' id="arrival_date_time" placeholder=""></p>
-            <p>{{ Form::label('departure_date', 'Departure Date &amp; Time') }}
-               <input type="datetime-local" class="form-control" name='departure_date_time' id="departure_date_time" placeholder=""></p>
+			 <div class="form-group">
+	            	<label for="start_date">Choose: Arrival Date &amp; Time </label>
+	                <div class='input-group date' id='datetimepicker1'>
+	                    <input type='text' class="form-control" name='arrival_date_time' id="arrival_date_time">
+	                    <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
+	                </div>
+            </div>
+             <div class="form-group">
+	            	<label for="start_date">Choose: Arrival Date &amp; Time </label>
+	                <div class='input-group date' id='datetimepicker2'>
+	                    <input type='text' class="form-control" name='departure_date_time' id="departure_date_time">
+	                    <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
+	                </div>
+            </div>
             <p>{{ Form::label('license_plate_number', 'License Plate Number') }}
             	<input type="text" class="form-control" name="license_plate_number" id="license_plate_number" placeholder="" required></p>
             	<br>
+           
+		   
+		    
 	        <p>{{ Form::submit('Submit', array('class' => 'btn btn-lg btn-primary btn-block')) }}</p>
 	    {{ Form::close() }}			
 </div> 
+@stop
+
+@section('bottom-script')
+<!-- Bower Components--> 
+<script type="text/javascript" src="/bower_components/moment/min/moment.min.js"></script>
+<script type="text/javascript" src="/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
+
+<script>
+	$(function () {
+		$('#datetimepicker1').datetimepicker();
+	});
+	$(function () {
+		$('#datetimepicker2').datetimepicker();
+	});
+</script>
 @stop
 
