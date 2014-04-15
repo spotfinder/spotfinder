974b48c0188bb300d2c9adc39e0c453ee0da0cd7
KarinaMontesTrevino <kimontrevino@gmail.com>
2014-04-15 16:34:27 -0500

    admin function: add a lot

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -14,10 +14,11 @@ class AdminController extends BaseController {
 	 */
 	public function index()
 	{
-
-		$users = User::all();
-		return View::make('admin')->with(array('users'=> $users));
-
+       if (Auth::check())
+	   {
+			$users = User::all();
+			return View::make('admin')->with(array('users'=> $users));
+        }
 	}
 
 
@@ -88,7 +89,33 @@ class AdminController extends BaseController {
 	public function destroy($id)
 	{
 		//
+		$user = User::findOrFail($id);
+		$user->delete();
+		return Redirect::action('AdminController@index');
 	}
+    
+    public function addLot(){
 
+    	$lot = new Lot();
+
+    		$lot->lot_id = Input::get('lot_id');
+			$lot->lot_name = Input::get('lot_name');
+			$lot->area_id = Input::get('area_id');
+			$lot->area_name = Input::get('area_name');
+			$lot->street_address = Input::get('street_address');
+			$lot->city = Input::get('city');
+			$lot->zip = Input::get('zip');
+			$lot->phone_number = Input::get('phone_number');
+			$lot->capacity = Input::get('capacity');
+			$lot->open_time = Input::get('open_time');
+			$lot->close_time = Input::get('close_time');
+			$lot->latitude = Input::get('latitude');
+			$lot->longitude = Input::get('longitude');
+			$lot->cost_per_hour = Input::get('cost_per_hour');
+			$lot->save();
+
+			Session::flash('successMessage', 'Lot added successfully');
+			return Redirect::action('AdminController@index');
+    }
 
 }
--- app/views/admin.blade.php
+++ app/views/admin.blade.php
@@ -9,7 +9,7 @@
         .col-sm-12{
             margin-left: 90px;
             text-align: center;
-            width: 900px;
+            width: 1200px;
         }
         body {
             background-color: #FFF;
@@ -18,12 +18,73 @@
     </style>
 @stop
 @section('content')
-    <div class="row">
+<!--Users Table-->
+   <div class="row">
+          <div class="col-sm-12">
+            <div class="panel panel-primary">
+              <div class="panel-heading">
+                <h3 class="panel-title"><i class="fa fa-users"></i> Existing Users</h3>
+              </div>
+              <div class="panel-body">
+                <div class="table-responsive">
+                  <table class="table table-bordered table-hover table-striped tablesorter">
+                    <thead>
+                      <tr>
+                        <th>First Name<i class="fa fa-sort"></i></th>
+                        <th>Last Name <i class="fa fa-sort"></i></th>
+                        <th>Email <i class="fa fa-sort"></i></th>
+                        <th>Created At <i class="fa fa-sort"></i></th>
+                        <th>Updated At <i class="fa fa-sort"></i></th>
+                        <th>Role Id <i class="fa fa-users"></i></th>
+                        <th>Edit <i class="fa fa-pencil-square-o"></i></th>
+                        <th>Delete <i class="fa fa-trash-o"></i></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                       @foreach ($users as $user)
+                        <tr>
+                            <td>
+                                {{$user->first_name}}
+                            </td>
+                            <td>
+                                {{$user->last_name}}
+                            </td>
+                            <td>
+                                {{$user->email}}
+                            </td>
+                            <td>
+                                {{$user->created_at}}
+                            </td>
+                            <td>
+                                {{$user->updated_at}}
+                            </td>
+                             <td>
+                                {{$user->role_id}}
+                            </td>
+                            <td>
+                                <i class="fa fa-pencil-square-o"></i>
+                            </td>
+                            <td>
+                                <a href = "#" class = "delete_user"><i class="fa fa-trash-o"></i></a>
+                                {{ Form::open(array('action'=> array('AdminController@destroy', $user->id), 'method' => 'delete', 'id' => 'formDeleteUser'))}}
+                                {{ Form::close() }} 
+                            </td>
+                        </tr>
+                       @endforeach
+                    </tbody>
+                 </table>
+               </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Add Lots Table-->
+<div class="row">
           <div class="col-sm-12">
             <div class="panel panel-primary">
                 <div class="panel-heading"><h3 class="panel-title"><i class="fa fa-road"></i> Add a Lot</h3></div>
                   <div class="panel-body">
-                   <form>
+                   {{ Form::open(array('action' => 'AdminController@addLot', 'class' => 'form', 'role'=>'form', 'method'=> 'post')) }}
                         <div class="form-group">
                             <label>Lot Id</label>
                             <input type="number" name="lot_id" id='lot_id'>
@@ -54,76 +115,20 @@
                             <label>Close Time</label>
                             <input type="time" name="close_time" id="close_time">
                             <label>Latitude</label>
-                            <input type="float" name="latituted" id="latitude">
+                            <input type="float" name="latitude" id="latitude">
                         </div>
                         <div class="form-group"> 
-                        <label>Longitude</label>
-                        <input type="float" name="longitude" id="longitude">
-                        <label>Cost Per Hour</label>
-                        <input type="number"  min="0.01" step="0.01" name="cost_per_hr" id="cost_per_hr">
+                            <label>Longitude</label>
+                            <input type="float" name="longitude" id="longitude">
+                            <label>Cost Per Hour</label>
+                            <input type="number"  min="0.01" step="0.01" name="cost_per_hour" id="cost_per_hour">
                         </div>
                         <input type="submit" value="Add Lot" class="btn btn-primary">
-                    </form>
+                    {{ Form::close() }}
                   </div>
             </div>
         </div>
     </div>
-
-
-
-   <div class="row">
-          <div class="col-sm-12">
-            <div class="panel panel-primary">
-              <div class="panel-heading">
-                <h3 class="panel-title"><i class="fa fa-users"></i> Existing Users</h3>
-              </div>
-              <div class="panel-body">
-                <div class="table-responsive">
-                  <table class="table table-bordered table-hover table-striped tablesorter">
-                    <thead>
-                      <tr>
-                        <th>First Name<i class="fa fa-sort"></i></th>
-                        <th>Last Name <i class="fa fa-sort"></i></th>
-                        <th>Email <i class="fa fa-sort"></i></th>
-                        <th>Created At <i class="fa fa-sort"></i></th>
-                        <th>Updated At <i class="fa fa-sort"></i></th>
-                        <th>Edit <i class="fa fa-pencil-square-o"></i></th>
-                        <th>Delete <i class="fa fa-trash-o"></i></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                       @foreach ($users as $user)
-                        <tr>
-                            <td>
-                                {{$user->first_name}}
-                            </td>
-                            <td>
-                                {{$user->last_name}}
-                            </td>
-                            <td>
-                                {{$user->email}}
-                            </td>
-                            <td>
-                                {{$user->created_at}}
-                            </td>
-                            <td>
-                                {{$user->updated_at}}
-                            </td>
-                            <td>
-                                <i class="fa fa-pencil-square-o"></i>
-                            </td>
-                            <td>
-                                <i class="fa fa-trash-o"></i> 
-                            </td>
-                        </tr>
-                       @endforeach
-                    </tbody>
-                 </table>
-               </div>
-            </div>
-        </div>
-    </div>
-</div>
 
 @stop
 
@@ -133,6 +138,13 @@
     $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("active");
+    });
+
+    $('.delete_user').on('click', function(e){
+        e.preventDefault();
+        if(confirm("Are you sure you want to delete this user?")){
+         $('#formDeleteUser').submit();   
+        }
     });
     </script>
 @stop
