a1423113b708f738550a0626eb745f6ff85a14fc
miketamburo <matsatam@gmail.com>
2014-04-18 16:40:50 -0500

    update for DB and server

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,15 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
+// $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+// 	'local' => array('your-machine-name'),
 
-));
+// ));
+
+$env = $app->detectEnvironment(function() {
+	return !empty($_SERVER['LARAVEL_ENV']) && $_SERVER['LARAVEL_ENV'] == 'local' ? 'local' : 'production';
+});
 
 /*
 |--------------------------------------------------------------------------
